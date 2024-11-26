603c8793306c5c48909b18db8d798047d3349925
Guiton1er <bahiergael@gmail.com>
2024-11-26 13:47:11 +0100

    trying to deploy

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,9 @@
+import './styles/bootstrap.scss';
+
 import './styles/app.scss';
+
 import * as bootstrap from 'bootstrap';
+
+import "./scripts/script.js";
+
+import '@fortawesome/fontawesome-free/js/all.min';
